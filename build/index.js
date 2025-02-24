35cd38de34cdb78d86a05cbd5f93018569ceea64
Ana Jaqueline <jaquelinesales200729@gmail.com>
2025-02-24 10:14:09 -0300

    inicio dos estudos con angular

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function printaObjeto(pessoa) {
-    console.log(pessoa);
-}
-printaObjeto({
-    name: "Bruce Uene",
-    vulgo: "Batman"
-});
-console.log("ola mundo");
--- frameworks/typescript-para-angular/src/index.ts
+++ frameworks/typescript-para-angular/src/index.ts
@@ -1,16 +1,110 @@
-type heroi = {
-    name: string,
-    vulgo: String
+/*let ligado:boolean = false
+let nome:string = "Jaqueline"
+let idade:number = 17
+let altura:number = 1.50
+
+let nulo:null = null
+let indefinido:undefined = undefined
+
+//O void é usado para executar coisas que não retornam nada
+let retorn:void
+//o any aceita qualquer tipo de valor
+let any:any = ''
+
+let produto:object ={
+    name: "Jaqueline",
+    cidade:"maranguape",
+    idade: 17,
+}
+
+class Character {
+    protected name?: string;
+    readonly stregth: number;
+    skill: number;
+
+    constructor(name: string, stregth: number, skill: number) {
+        this.name = name;
+        this.stregth = stregth;
+        this.skill = skill;
+    }
+
+    attack(): void{
+        console.log(`Atack with ${this.stregth} points`)
+    }
+}
+
+//Character: superclass
+//Magician : subclass
+class Magician extends Character{
+
+    magicPoints: number;
+    constructor(name: string, stregth: number, skill:number, magicPoints: number){
+        super(name, stregth, skill)
+        this.magicPoints = magicPoints;
+    }
+}
+
+//generics
+
+
+function concatArray<T>(...itens: T[]): T[]{
+    return new Array().concat(...itens)
+}
+
+const numArray = concatArray<number[]>([1,5],[3])
+const stgArray = concatArray<string[]>(["Jaqueline"],["Cucas"])
+
+//decorators
+
+function ExibirNome(target: any){
+    console.log(target)
+}
+
+@ExibirNome
+class Funcionario{
+
+}
+
+@ExibirNome
+class Quincas{
+
+}
+function apiVersion(version: string){
+    return(target:any)=>{
+        Object.assign(target.prototype,{
+            __version: version
+        })
+    }
 }
 
 
-function printaObjeto(pessoa: heroi){
-    console.log(pessoa);
+@apiVersion("1.10")*/
+function minLength(length: number){
+    return (target:any, key:string) => {
+        let _value = target[key]
+
+        const getter = () => _value
+        const setter = (value:string) =>{
+            if(value.length<length){
+                throw new Error(`Tamanho menor do que ${length}`)
+            }else{
+                _value = value
+            }
+        }
+
+        Object.defineProperty(target, key,{
+            get: getter,
+            set: setter,
+        })
+    }}
+
+class Api{
+    @minLength(3)
+    name:string
+    constructor(name:string){
+        this.name = name
+    }
 }
-
-printaObjeto({
-    name: "Bruce Uene",
-    vulgo: "Batman"
-})
-
-console.log("ola mundo");
+/*
+const api = new Api();
+console.log(api.__version)*/
