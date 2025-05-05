934ac6024f9b9700b142c21c692cd1ae3323126e
Ana Jaqueline <jaquelinesales200729@gmail.com>
2025-05-04 21:11:03 -0300

    aula 3

--- ide-java/meu-primeiro-projeto-java/src/MinhaClasse.java
+++ ide-java/meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -2,5 +2,16 @@ public class MinhaClasse {
 
     public static void main (String [] args){
         System.out.print("Olá mundo");
+
+        String primeiroNome = "Jaqueline";
+        String segundoNome = "Sales";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultdo do método:" + primeiroNome.concat(" ").concat(segundoNome);
+        
     }
 }
