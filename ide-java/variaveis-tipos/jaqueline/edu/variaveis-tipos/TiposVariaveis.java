7a04c94fa9a51bbe60692d7837e89a16b011545e
Ana Jaqueline <jaquelinesales200729@gmail.com>
2025-05-12 17:23:40 -0300

    alterado Java para java

---
+++ ide-java/variaveis-tipos/jaqueline/edu/variaveis-tipos/TiposVariaveis.java
@@ -0,0 +1,14 @@
+
+
+public class TiposVariaveis{
+
+    public static void main(String[] args) throws Exception {
+
+        String meuNome = "Jaqueline";
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.output.printr(VALOR_DE_PI)
+
+    }
+}
